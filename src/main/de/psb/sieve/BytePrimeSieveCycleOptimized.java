7fcd478c6484b2213a4f3ea83f8edb48a73f673f
maucon <maurice.nocon@adesso.de>
2020-04-22 12:53:46 +0200

    added BytePrimeSieveCycleOptimized

---
+++ src/main/de/psb/sieve/BytePrimeSieveCycleOptimized.java
@@ -0,0 +1,31 @@
+package de.psb.sieve;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BytePrimeSieveCycleOptimized implements IPrimeSieve {
+
+    @Override
+    public List<Integer> computePrimes(int limit) {
+        List<Integer> primeList = new ArrayList<>();
+
+        byte[] byteSieve = new byte[limit / 8 + 1];
+        int sqrt = (int) Math.sqrt(limit);
+
+        if (limit >= 2) primeList.add(2);
+
+        for (int i = 3; i <= sqrt; i += 2)
+            if ((byteSieve[i / 8] & (1 << (i % 8))) == 0) {
+                primeList.add(i);
+                for (long j = i * i; j <= limit; j += i << 1)
+                    byteSieve[(int) j / 8] = (byte) (byteSieve[(int) j / 8] | (1 << (j % 8)));
+            }
+
+        for (int i = (sqrt & 1) == 0 ? sqrt + 1 : sqrt + 2; i <= limit; i += 2)
+            if ((byteSieve[i / 8] & (1 << (i % 8))) == 0)
+                primeList.add(i);
+
+        return primeList;
+    }
+
+}
