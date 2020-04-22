627073f5cb4db6046f4b57f973198ebfd00c3cbe
maucon <maurice.nocon@adesso.de>
2020-04-22 11:36:17 +0200

    renamed, edited Test

--- src/test/de/psb/SieveTest.java
+++ src/test/de/psb/SieveTest.java
@@ -3,15 +3,18 @@ package de.psb;
 
 import de.psb.sieve.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class Test {
+public class SieveTest {
 
     private IPrimeSieve[] primeSieve;
     private Map<Integer, Integer> primeSieveTest;
 
+    @BeforeAll
     public void initialize() {
         primeSieve = new IPrimeSieve[]{
                 new EratosthenesSieve(),
@@ -20,7 +23,6 @@ public class Test {
                 new BytePrimeSieveFullOptimized()
         };
 
-        primeSieveTest = new HashMap<>();
         primeSieveTest = new HashMap<>();
         primeSieveTest.put(10, 4);
         primeSieveTest.put(13, 6);
@@ -35,24 +37,28 @@ public class Test {
         primeSieveTest.put(100_000_000, 5761455);
     }
 
+    @Test
     public void testEratosthenesSieve() {
         for (int key : primeSieveTest.keySet()) {
             Assertions.assertEquals(primeSieve[0].computePrimes(key).size(), (int) primeSieveTest.get(key));
         }
     }
 
+    @Test
     public void testEratosthenesSieveCycleOptimized() {
         for (int key : primeSieveTest.keySet()) {
             Assertions.assertEquals(primeSieve[1].computePrimes(key).size(), (int) primeSieveTest.get(key));
         }
     }
 
+    @Test
     public void testEratosthenesSieveFullOptimized() {
         for (int key : primeSieveTest.keySet()) {
             Assertions.assertEquals(primeSieve[2].computePrimes(key).size(), (int) primeSieveTest.get(key));
         }
     }
 
+    @Test
     public void testBytePrimeSieveFullOptimized() {
         for (int key : primeSieveTest.keySet()) {
             Assertions.assertEquals(primeSieve[3].computePrimes(key).size(), (int) primeSieveTest.get(key));
